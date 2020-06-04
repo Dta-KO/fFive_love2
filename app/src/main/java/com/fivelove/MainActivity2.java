f48d2d8a5e61e36c073882af559ca1b59a7060ac
DTA_KO <dtako.developer@gmail.com>
2020-06-04 09:10:45 +0700

    Initial commit

---
+++ app/src/main/java/com/fivelove/MainActivity2.java
@@ -0,0 +1,20 @@
+package com.fivelove;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.fivelove.databinding.ActivityMain2Binding;
+
+public class MainActivity2 extends AppCompatActivity {
+    ActivityMain2Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+
+}
---
+++ app/src/main/java/com/fivelove/OnClickButton.java
@@ -0,0 +1,12 @@
+package com.fivelove;
+
+import android.view.View;
+
+/**
+ * Created by Nguyen Kim Khanh on 6/4/2020.
+ */
+public class OnClickButton {
+    private void onClickBtnBlue(View view){
+
+    }
+}
