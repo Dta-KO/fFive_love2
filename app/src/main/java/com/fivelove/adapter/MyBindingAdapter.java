ef0e475a66a113e78accabbc3d3dafbbe9a8cc19
DTA_KO <dtako.developer@gmail.com>
2020-06-19 19:09:53 +0700

    update something

--- app/src/main/java/com/fivelove/adapter/MyBindingAdapter.java
+++ app/src/main/java/com/fivelove/adapter/MyBindingAdapter.java
@@ -10,10 +10,10 @@ import de.hdodenhof.circleimageview.CircleImageView;
  * Created by Nguyen Kim Khanh on 6/18/2020.
  */
 public class MyBindingAdapter {
-//    @BindingAdapter("android:src")
-//    public static void setImage(CircleImageView view, int resource) {
-//        Log.d("abc", "" + view.getHeight());
-//        Log.d("width", "" + view.getWidth());
-//        view.setImageResource(resource);
-//    }
+    @BindingAdapter("android:src")
+    public static void setImage(CircleImageView view, int resource) {
+        Log.d("abc", "" + view.getHeight());
+        Log.d("width", "" + view.getWidth());
+        view.setImageResource(resource);
+    }
 }
